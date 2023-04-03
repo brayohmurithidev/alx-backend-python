ae58878fd09d2e70f57424a29884eb9b6750a944
brayohmurithidev <murithibrianm@gmail.com>
2023-04-03 21:48:17 +0300

    waiting for a random number delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+'''
+A program that demos the async await, the await time is a
+float generated from a random uniform module.
+'''
 import random
 import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    '''Assign variable x to a random float and set it
+    as wait time then return it'''
     # print("Code execution starts")
     x = random.uniform(0, max_delay)
     await asyncio.sleep(x)
